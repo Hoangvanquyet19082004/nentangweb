e0bd0f558e9fda22a9f7ffd63d59801f79f16c9a
Quyet <hquyet190804@gmail.com>
2025-08-03 16:13:05 +0700

    su dung jquerry

---
+++ JQuerry/de_1/data.js
@@ -0,0 +1,32 @@
+const employees = [
+  {
+    name: "Hoang Van Quyet",
+    email: "hvq@mail.com",
+    address: "Ha Noi",
+    phone: "0123456789",
+  },
+  {
+    name: "Vu Xuan Huy",
+    email: "vxh@mail.com",
+    address: "Hai Duong",
+    phone: "0987654321",
+  },
+  {
+    name: "Vu Quang Khai",
+    email: "vqk@mail.com",
+    address: "Quang Ninh",
+    phone: "0912345678",
+  },
+  {
+    name: "Nguyen Quang Thanh",
+    email: "nqt@mail.com",
+    address: "Soc Son",
+    phone: "0901122334",
+  },
+  {
+    name: "Nguyen Van Quang",
+    email: "nvq@mail.com",
+    address: "Bac Ninh",
+    phone: "0967854321",
+  },
+];
---
+++ JQuerry/de_3/data.js
@@ -0,0 +1,26 @@
+var employees = [
+  {
+    name: "Mai",
+    surname: "Thục Anh",
+    address: "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    active: true
+  },
+  {
+    name: "Hoàng",
+    surname: "Sinh Hồng",
+    address: "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    active: false
+  },
+  {
+    name: "Tính",
+    surname: "Mai Trung",
+    address: "1411 Đường Láng, Cầu Giấy, Hà Nội",
+    active: true
+  },
+  {
+    name: "Hồng",
+    surname: "Nguyễn Văn",
+    address: "1411 Đường Láng, Cầu Giấy, Hà Nội",
+    active: false
+  }
+];
